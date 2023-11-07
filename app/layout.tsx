740cad86a0bd2ad6badf70ce26f0cf1f126346f0
Tilto_ <me@tilto.kr>
2023-11-07 11:45:17 +0900

    🎉 Initial Project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Tilto - 인터넷의 바다를 떠돌면서',
+  description: 'Tilto의 개발 블로그입니다.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
+      가끔은 휴식도 필요한 법이니까
+    </main>
+  );
+}
